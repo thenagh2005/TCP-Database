325f0abd61fcd030783b4d06f15e69d802f95e11
thenagh2005 <anaghpandya@gmail.com>
2025-12-17 07:43:32 +0530

    Further changes

--- cpp-db-backend/src/net/tcp_server.cpp
+++ cpp-db-backend/src/net/tcp_server.cpp
@@ -11,7 +11,7 @@
 
 namespace kv {
 
-    TCPServer::TCPServer(int port, int num_shards) : port_(port), store_(num_shards), server_sock_(-1) {
+    TCPServer::TCPServer(int port, int num_shards) : port_(port), store_(num_shards), server_sock_(-1), running_(false) {
         server_sock_ = socket(AF_INET, SOCK_STREAM, 0);
         if (server_sock_ < 0) {
             throw std::runtime_error("Failed to create socket");
@@ -38,6 +38,58 @@ namespace kv {
         }
         std::cout << "Server listening on port " << port_ << std::endl;
 
+    }
+
+    void TCPServer::start() { //Start the server
+        running_ = true;
+        std::cout << "Server started, waiting for connections..." << std::endl;
+        while (running_) {
+            struct sockaddr_in client_addr;
+            socklen_t client_len = sizeof(client_addr);
+
+            int client_sock = accept(server_sock_, (struct sockaddr*)&client_addr, &client_len); //Accept an incoming connection
+
+            if (client_sock < 0) {
+                //SOMETHING WENT WRONG AAAAHHHHH
+                if (running_) {
+                    std::cerr << "Failed to accept connection" << std::endl;
+                }
+                
+                continue;
+            }
+
+            //Now do stuff with the connection
+
+            std::cout << "Accepted connection from " << inet_ntoa(client_addr.sin_addr) << ":" << ntohs(client_addr.sin_port) << std::endl;
+            threads_.emplace_back(&TCPServer::handle_client, this, client_sock); //Handle the client in a new thread
+
+            
+
+
+        }
+    }
+
+    void TCPServer::stop() {
+        running_ = false;
+        close(server_sock_);
+        server_sock_ = -1;
+
+        for (auto &t : threads_) {
+            if (t.joinable()) {
+                t.join();
+            }
+        }
+
+        threads_.clear();
+        std::cout << "Server stopped." << std::endl;
+    }
+
+    TCPServer::~TCPServer() {
+        if (server_sock_ >= 0) {
+            stop();
+
+            //I will close server sock in stop() function
+        }
     }
 
 }
